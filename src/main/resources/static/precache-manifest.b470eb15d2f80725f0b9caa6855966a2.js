359d7aadfe38ae71ca1a0cb772745218036dd3dc
Brunon <lembrunon@gmail.com>
2019-08-02 19:02:33 +0200

    add react client

---
+++ src/main/resources/static/precache-manifest.b470eb15d2f80725f0b9caa6855966a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908109feddba7e0890c5389b3b6bf51a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "798fa76302375c8edef4",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "37c06356ca67ef57088d",
+    "url": "/static/css/main.abdc95a3.chunk.css"
+  },
+  {
+    "revision": "798fa76302375c8edef4",
+    "url": "/static/js/2.0baba427.chunk.js"
+  },
+  {
+    "revision": "37c06356ca67ef57088d",
+    "url": "/static/js/main.b6c4a9a9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
